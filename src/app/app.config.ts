e0cc44e22e1e95397942fd601f32c25475db4937
amuli4 <abimulia@outlook.com>
2024-11-17 14:23:59 +0700

    add auth expired interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,25 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimations } from "@angular/platform-browser/animations"
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  provideHttpClient,
+  withInterceptors,
+  withXsrfConfiguration,
+} from '@angular/common/http';
+import { authExpired } from './core/auth/auth-expired.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideAnimations()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authExpired]),
+      withXsrfConfiguration({
+        cookieName: 'XSRF-TOKEN',
+        headerName: 'X-XSRF-TOKEN',
+      })
+    ),
+    provideAnimations(),
+  ],
 };
