36cdf54b3e9f8250b02b1c4b52bd7fcce9d75086
amuli4 <abimulia@outlook.com>
2024-11-19 12:27:38 +0700

    add info step

--- src/app/landlord/properties-create/properties-create.component.ts
+++ src/app/landlord/properties-create/properties-create.component.ts
@@ -18,11 +18,12 @@ import { PriceVO } from '../model/listing-vo.model';
 import { CategoryStepComponent } from "./step/category-step/category-step.component";
 import { FooterStepComponent } from '../../shared/footer-step/footer-step.component';
 import { LocationStepComponent } from './step/location-step/location-step.component';
+import { InfoStepComponent } from './step/info-step/info-step.component';
 
 @Component({
   selector: 'app-properties-create',
   standalone: true,
-  imports: [CategoryStepComponent,FooterStepComponent,LocationStepComponent],
+  imports: [CategoryStepComponent,FooterStepComponent,LocationStepComponent,InfoStepComponent],
   templateUrl: './properties-create.component.html',
   styleUrl: './properties-create.component.scss',
 })
